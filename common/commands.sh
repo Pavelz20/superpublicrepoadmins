73ee2fe82230151edc3f99cf6dd04f806a0eaacf
Pavel <pavel.zavodyan@mail.ru>
2026-01-27 17:59:15 +0300

    fix

--- common/commands.sh
+++ common/commands.sh
@@ -3,12 +3,4 @@ Acquire::http::Proxy "http://proxy.tech.skills:3128";
 Acquire::https::Proxy "http://proxy.tech.skills:3128";
 EOF
 
-cat > /etc/apt/sources.list.d/aldpro.list << 'EOF'
-deb https://dl.astralinux.ru/aldpro/frozen/01/3.0.0/ 1.7_x86-64 main base
-EOF
-
-cat > /etc/apt/sources.list << 'EOF'
-deb http://dl.astralinux.ru/astra/frozen/1.7_x86-64/1.7.7/repository-main/ 1.7_x86-64 main contrib non-free
-deb http://dl.astralinux.ru/astra/frozen/1.7_x86-64/1.7.7/repository-update/ 1.7_x86-64 main contrib non-free
-EOF
-
+wget http://10.150.0.200/extra/{core-snap.zip,core20-snap.zip,microk8s-snap.zip}
