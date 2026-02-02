337e1c98c4ff2ba265daed55332a9895c43a2c85
Pavel <pavel.zavodyan@mail.ru>
2026-02-03 00:26:15 +0300

    fix

--- common/commands.sh
+++
@@ -1,6 +0,0 @@
-cat > /etc/apt/apt.conf.d/90-proxy.conf << 'EOF'
-Acquire::http::Proxy "http://proxy.tech.skills:3128";
-Acquire::https::Proxy "http://proxy.tech.skills:3128";
-EOF
-
-wget http://10.150.0.200/extra/{core-snap.zip,core20-snap.zip,microk8s-snap.zip}
